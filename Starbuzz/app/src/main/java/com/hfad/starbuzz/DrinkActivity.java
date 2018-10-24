61ca533b947264c1e13082f38fdbfaa0085e55c2
t3mp0ralflux <t3mp0ralflux@gmail.com>
2018-10-23 20:04:34 -0400

    Adding up to chapter 9

---
+++ Starbuzz/app/src/main/java/com/hfad/starbuzz/DrinkActivity.java
@@ -0,0 +1,30 @@
+package com.hfad.starbuzz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DrinkActivity extends Activity {
+
+    public static final String EXTRA_DRINKID= "drinkId";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink);
+
+        int drinkId = (Integer) getIntent().getExtras().get(EXTRA_DRINKID); //gotta type-cast this beast as it's passed as a string.  need an integer
+        Drink drink = Drink.drinks[drinkId];
+
+        TextView name = findViewById(R.id.name);
+        name.setText(drink.getName());
+
+        TextView description = findViewById(R.id.description);
+        description.setText(drink.getDescription());
+
+        ImageView photo = findViewById(R.id.photo);
+        photo.setContentDescription(drink.getName());
+        photo.setImageResource(drink.getImageResourceId());
+
+    }
+}
